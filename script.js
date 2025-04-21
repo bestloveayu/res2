42f8f76552e798cb358ef046ecd9bcf8c4db2b33
bestloveayu <bestloveayu@gmail.com>
2025-04-22 01:22:05 +0800

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function verifyPasscode() {
     const message = document.getElementById("message");
 
     if (passcode === correctPasscode) {
-       const newWindow = window.open("https://sites.google.com/view/resres3/%E9%A6%96%E9%A0%8", "_blank");
+       const newWindow = window.open("https://sites.google.com/view/resres3/%E9%A6%96%E9%A0%81", "_blank");
         if (!newWindow || newWindow.closed || typeof newWindow.closed == "undefined") {
             message.textContent = "請允許彈出式視窗以進入下一關！";
         }
