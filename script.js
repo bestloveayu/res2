920db8b57748fa05de12303d6be7190089fed1a2
bestloveayu <bestloveayu@gmail.com>
2025-04-22 01:19:09 +0800

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function verifyPasscode() {
     const message = document.getElementById("message");
 
     if (passcode === correctPasscode) {
-        window.location.href = "https://sites.google.com/view/resres3/%E9%A6%96%E9%A0%81"; // 與第三位人物交談
+       const newWindow = window.open("https://sites.google.com/view/resres3/%E9%A6%96%E9%A0%8", "_blank");
+        if (!newWindow || newWindow.closed || typeof newWindow.closed == "undefined") {
+            message.textContent = "請允許彈出式視窗以進入下一關！";
+        }
     } else {
         message.textContent = "不要亂猜!再好好整理重點吧!";
     }
